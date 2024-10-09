12c50409d486370ba15dace8ed90afd43d6b6c95
Razi <zirachwidh@gmail.com>
2024-10-10 00:15:52 +0700

    refactor: Add Comments

--- src/ADTMatrix/Matrix.java
+++ src/ADTMatrix/Matrix.java
@@ -9,263 +9,279 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+/* Definisi Kelas Matrix */
 public class Matrix 
 {
-  public static Scanner input = new Scanner(System.in);
-  public int rowEff;
-  public int colEff;
-  public double [][] matrix;
+    /* Atribut */
+    public static Scanner input = new Scanner(System.in);
+    public int rowEff;
+    public int colEff;
+    public double [][] matrix;
 
-  public Matrix(int mRow, int nCol) 
-  {
-      this.rowEff = mRow;
-      this.colEff = nCol;
-      this.matrix = new double[mRow][nCol];
-  }
+    /* Konstruktor */
+    public Matrix(int mRow, int nCol) 
+    {
+        // Membuat matriks berukuran mRow x nCol
+        this.rowEff = mRow;
+        this.colEff = nCol;
+        this.matrix = new double[mRow][nCol];
+    }
 
-  public Matrix()
-  {
-      this.matrix = new double[100][100]; 
-      this.rowEff = 0;
-      this.colEff = 0;
-  }
+    public Matrix()
+    {
+        // Membuat matriks berukuran 100 x 100 (default)
+        this.matrix = new double[100][100]; 
+        this.rowEff = 0;
+        this.colEff = 0;
+    }
 
-  private boolean isValidRowInput(String[] rowElmts, int nCol) 
-  {
-      if (rowElmts.length != nCol) 
-      {
-          System.out.println("Jumlah bilangan tidak sesuai banyak kolom, masukkan " + nCol + " buah bilangan.");
-          return false;
-      }
+    /* Validator */
+    private boolean isValidRowInput(String[] rowElmts, int nCol) 
+    {
+        if (rowElmts.length != nCol) 
+        {
+            System.out.println("Jumlah bilangan tidak sesuai banyak kolom, masukkan " + nCol + " buah bilangan.");
+            return false;
+        }
 
-      try 
-      {
-          for (String element : rowElmts) 
-          {
-              Double.parseDouble(element);
-          }
-      } 
-      catch (NumberFormatException e) 
-      {
-          System.out.println("Terdapat input yang tidak valid, mohon ulangi input.");
-          return false;
-      }
+        try 
+        {
+            for (String element : rowElmts) 
+            {
+                Double.parseDouble(element);
+            }
+        } 
+        
+        catch (NumberFormatException e) 
+        {
+            System.out.println("Terdapat input yang tidak valid, mohon ulangi input.");
+            return false;
+        }
 
-      return true;
-  }
+        return true;
+    }
 
-  public void readMatrix(int mRow, int nCol) 
-  {
-      this.rowEff = mRow;
-      this.colEff = nCol;
+    /* IO */
+    public void readMatrix(int mRow, int nCol) 
+    {
+        // Membaca matriks dari input keyboard user
+        this.rowEff = mRow;
+        this.colEff = nCol;
 
-      Scanner input = new Scanner(System.in);
-      for (int row = 0; row < mRow; row++) 
-      {
-          boolean valid = false;
-          do 
-          {
-              String line = input.nextLine();
-              String[] rowElmts = line.split(" ");
-              
-              if (isValidRowInput(rowElmts, this.colEff)) 
-              {
-                  for (int col = 0; col < this.colEff; col++) 
-                  {
-                      this.matrix[row][col] = Double.parseDouble(rowElmts[col]);
-                  }
-                  valid = true;
-              }
-          } 
-          while (!valid);
-      }
-      input.close();
-  }
+        Scanner input = new Scanner(System.in);
+        for (int row = 0; row < mRow; row++) 
+        {
+            boolean valid = false;
+            do 
+            {
+                String line = input.nextLine();
+                String[] rowElmts = line.split(" ");
+                
+                if (isValidRowInput(rowElmts, this.colEff)) 
+                {
+                    for (int col = 0; col < this.colEff; col++) 
+                    {
+                        this.matrix[row][col] = Double.parseDouble(rowElmts[col]);
+                    }
+                    valid = true;
+                }
+            } 
+            while (!valid);
+        }
+        input.close();
+    }
 
-  public void writeMatrix()
-  {
-    int[] columnWidths = new int[this.colEff];
-    
-    // Find max width for each col
-    for (int col = 0; col < this.colEff; col++) {
-        for (int row = 0; row < this.rowEff; row++) {
-            
-            int width = String.format("%.5f", this.matrix[row][col]).length();
-            
-            if (width > columnWidths[col]) {
-                columnWidths[col] = width;
+    public void writeMatrix()
+    {
+        // Menuliskan matriks ke layar
+        int[] columnWidths = new int[this.colEff];
+
+        for (int col = 0; col < this.colEff; col++) 
+        {
+            for (int row = 0; row < this.rowEff; row++) 
+            {
+                int width = String.format("%.5f", this.matrix[row][col]).length();
+                
+                if (width > columnWidths[col]) 
+                {
+                    columnWidths[col] = width;
+                }
             }
         }
-    }
 
-    // Pretty Print :)
-    for (int row = 0; row < this.rowEff; row++)
-    {
-      if (this.rowEff == 1)
-      {
-        System.out.print("[");
-      }
-      else if (row == 0)
-      {
-        System.out.print("┌");
-      }
-      else if (row == this.rowEff - 1)
-      {
-        System.out.print("└");
-      }
-      else
-      {
-        System.out.print("|");
-      }
-
-      for (int col = 0; col < this.colEff; col++) {
-          System.out.printf("%" + (columnWidths[col] + 2) + ".5f", this.matrix[row][col]); 
-      }
-
-      if (this.rowEff == 1)
-      {
-        System.out.print("  ]");
-      }
-      else if (row == 0)
-      {
-        System.out.print("  ┐");
-      }
-      else if (row == this.rowEff - 1)
-      {
-        System.out.print("  ┘");
-      }
-      else
-      {
-        System.out.print("  |");
-      }
-
-      System.out.println();
-    }
-  }
-
-  public void importMatrix(String filename)
-  {
-    File file = new File(filename);
-
-    try
-    {
-      BufferedReader reader = new BufferedReader(new FileReader(file));
-      String line;
-
-      while ((line = reader.readLine()) != null)
-      {
-        String[] rowElmts = line.split(" ");
-        
-        if (rowElmts.length > 0)
+        // Pretty Print :)
+        for (int row = 0; row < this.rowEff; row++)
         {
-          if (this.colEff == 0)
-          {
-            this.colEff = rowElmts.length;
-          }
+            if (this.rowEff == 1)
+            {
+                System.out.print("[");
+            }
+            else if (row == 0)
+            {
+                System.out.print("┌");
+            }
+            else if (row == this.rowEff - 1)
+            {
+                System.out.print("└");
+            }
+            else
+            {
+                System.out.print("|");
+            }
 
-          if (isValidRowInput(rowElmts, this.colEff))
-          {
-              for (int col = 0; col < this.colEff; col++) 
-              {
-                  this.matrix[this.rowEff][col] = Double.parseDouble(rowElmts[col]);
-              }
-              this.rowEff++;
-          } 
+            for (int col = 0; col < this.colEff; col++) 
+            {
+                System.out.printf("%" + (columnWidths[col] + 2) + ".5f", this.matrix[row][col]); 
+            }
+
+            if (this.rowEff == 1)
+            {
+                System.out.print("  ]");
+            }
+            else if (row == 0)
+            {
+                System.out.print("  ┐");
+            }
+            else if (row == this.rowEff - 1)
+            {
+                System.out.print("  ┘");
+            }
+            else
+            {
+                System.out.print("  |");
+            }
+
+            System.out.println();
         }
-      }
-      reader.close();
     }
 
-    catch (FileNotFoundException e)
+    public void importMatrix(String filename)
     {
-      System.out.println("File tidak ditemukan.");
-    }
+        // Membaca matriks dari file
+        File file = new File(filename);
 
-    catch (IOException e)
-    {
-      System.out.println("Terjadi kesalahan saat membaca file.");
-    }
-  }
-
-  public void importMatrixWithEmpty(String filename, int nEmpty)
-  {
-    /* nEmpty */
-    /* Regresi Linier : nEmpty = 1
-     * Interpolasi    : nEmpty = 1
-     * Bikubik        : nEmpty = 2
-     */
-
-    File file = new File(filename);
-
-    try
-    {
-      Scanner fileReader = new Scanner(file);
-      int nElmt = 0;
-
-      while(fileReader.hasNextLine())
-      {
-        this.rowEff++;
-        Scanner rowReader = new Scanner(fileReader.nextLine());
-
-        while(rowReader.hasNextDouble())
+        try
         {
-          nElmt++;
-          rowReader.nextDouble();
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = reader.readLine()) != null)
+            {
+                String[] rowElmts = line.split(" ");
+                
+                if (rowElmts.length > 0)
+                {
+                    if (this.colEff == 0)
+                    {
+                        this.colEff = rowElmts.length;
+                    }
+
+                    if (isValidRowInput(rowElmts, this.colEff))
+                    {
+                        for (int col = 0; col < this.colEff; col++) 
+                        {
+                            this.matrix[this.rowEff][col] = Double.parseDouble(rowElmts[col]);
+                        }
+                        this.rowEff++;
+                    } 
+                }
+            }
+            reader.close();
         }
-        rowReader.close();
-      }
-      fileReader.close();
 
-      this.colEff = (nElmt + nEmpty) / this.rowEff;
-
-      fileReader = new Scanner(file);
-      for (int row = 0; row < this.rowEff; row++)
-      {
-        for (int col = 0; col < this.colEff; col++)
+        catch (FileNotFoundException e)
         {
-          if (col < this.colEff - nEmpty)
-          {
-            this.matrix[row][col] = fileReader.nextDouble();
-          }
-          else
-          {
-            this.matrix[row][col] = -999;
-          }
+            System.out.println("File tidak ditemukan.");
         }
-      }
-      fileReader.close();
-    }
 
-    catch (FileNotFoundException e)
-    {
-      System.out.println("File tidak ditemukan.");
-    }
-  }
-
-  public void exportMatrix(Matrix matrix)
-  {
-    String filename;
-    System.out.print("Masukkan nama file: ");
-    filename = input.nextLine() + ".txt";
-
-    try
-    {
-      BufferedWriter writer = new BufferedWriter(new FileWriter("./test/" + filename));
-
-      for (int row = 0; row < matrix.rowEff; row++)
-      {
-        for (int col = 0; col < matrix.colEff; col++)
+        catch (IOException e)
         {
-          writer.write(matrix.matrix[row][col] + " ");
+            System.out.println("Terjadi kesalahan saat membaca file.");
         }
-        writer.newLine();
-      }
-      writer.close();
     }
 
-    catch (IOException e)
+    public void importMatrixWithEmpty(String filename, int nEmpty)
     {
-      System.out.println("Terjadi kesalahan saat menulis file.");
+        // Membaca matriks dari file dengan elemen kosong sebanyak nEmpty
+
+        /* nEmpty */
+        /* Regresi Linier : nEmpty = 1
+        * Interpolasi    : nEmpty = 1
+        * Bikubik        : nEmpty = 2
+        */
+
+        File file = new File(filename);
+
+        try
+        {
+            Scanner fileReader = new Scanner(file);
+            int nElmt = 0;
+
+            while(fileReader.hasNextLine())
+            {
+                this.rowEff++;
+                Scanner rowReader = new Scanner(fileReader.nextLine());
+
+                while(rowReader.hasNextDouble())
+                {
+                    nElmt++;
+                    rowReader.nextDouble();
+                }
+                rowReader.close();
+            }
+            fileReader.close();
+
+            this.colEff = (nElmt + nEmpty) / this.rowEff;
+
+            fileReader = new Scanner(file);
+            for (int row = 0; row < this.rowEff; row++)
+            {
+                for (int col = 0; col < this.colEff; col++)
+                {
+                    if (col < this.colEff - nEmpty)
+                    {
+                        this.matrix[row][col] = fileReader.nextDouble();
+                    }
+                    else
+                    {
+                        this.matrix[row][col] = -999;
+                    }
+                }
+            }
+            fileReader.close();
+        }
+
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File tidak ditemukan.");
+        }
     }
-  }
-}
+
+    public void exportMatrix(Matrix matrix)
+    {
+        // Menulis matriks ke file
+        String filename;
+        System.out.print("Masukkan nama file: ");
+        filename = input.nextLine() + ".txt";
+
+        try
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("./test/" + filename));
+
+            for (int row = 0; row < matrix.rowEff; row++)
+            {
+                for (int col = 0; col < matrix.colEff; col++)
+                {
+                    writer.write(matrix.matrix[row][col] + " ");
+                }
+                writer.newLine();
+            }
+            writer.close();
+        }
+
+        catch (IOException e)
+        {
+            System.out.println("Terjadi kesalahan saat menulis file.");
+        }
+    }
+} 
